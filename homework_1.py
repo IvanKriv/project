3a65cf7beaa92a2427f4574fa1af807620640e01
Ivan Krivitskiy <66959037+IvanKriv@users.noreply.github.com>
2020-07-23 15:45:03 +0300

    Delete homework_1.py

--- homework_1.py
+++
@@ -1,8 +0,0 @@
-from sys import argv
-
-name, hours, salary, prize = argv
-hours = int(hours)
-salary = int(salary)
-prize = int(prize)
-
-print('Ваша заработная плата с учётом премии составит:', hours * salary + prize)
